2a2b7a0813f59cafa7daea8601e4f25bf6cae634
Keaton Burleson <keaton.burleson@er2.com>
2020-06-24 14:19:26 -0500

    Initial Commit

---
+++ SwiftDisks/SwiftDisks.h
@@ -0,0 +1,19 @@
+//
+//  SwiftDisks.h
+//  SwiftDisks
+//
+//  Created by Keaton Burleson on 6/24/20.
+//  Copyright © 2020 Keaton Burleson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftDisks.
+FOUNDATION_EXPORT double SwiftDisksVersionNumber;
+
+//! Project version string for SwiftDisks.
+FOUNDATION_EXPORT const unsigned char SwiftDisksVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftDisks/PublicHeader.h>
+
+
